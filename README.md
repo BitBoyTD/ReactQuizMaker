a389f3aa14b08945beb225583ef672a4b0fea12c
seandosborne <seandosborne@gmail.com>
2023-12-12 10:50:43 +1300

    add: about section and README file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Coming Soon...
+Welcome to react Quiz Maker, your all in one tool for creating, sharing and displaying quizzes. I am Seán Osborne and I started coding this react Quiz Maker site on the 7th of October 2023, but Quiz Maker started a while before that. I started working on the original Quiz Maker on the 24th of June 2023 using vanilla HTML, CSS and JS, mostly for the purpose of improving my knowlege of JS. In a similiar way, my reason for creating this one using React was to better my understanding of React.
+
+Aswell as this I wanted to create a tool to help me study. When revising I like to make quizzes to help me memorise information, but I would use tools like google docs or sheets to create them which wasn't great, so I decided to create a better way to do that. This idea turned into the Quiz Maker site that you see today.
+
+Using React for this project instead of vanilla JS has been a game changer. I have been able to get stuff done faster and to implement more advanced features than with the original Quiz Maker, such as the share feature and the sidemenu to name a few.
+
+The share feature I have realised can be used in a few interesting ways, thanks to the quizzes being shared in a purely text format. This allows users to put their quizzes in any file or form that contains text. So, in theory one could back up all their quizzes in a text file on their computer or in their google drive to be accessed from anywhere.
+
+Another cool implication of it being all text is that it means ChatGPT or other A.I. chatbots are able to create quizzes in the text format. For example you can ask ChatGPT to generate a quiz about capital cities, but to put it in the format of a Quiz Maker quiz (Make sure to paste in an example of the Quiz Maker format). I have tried it myself and it works fantastically. It means that you could paste your study material or an already made quiz into ChatGPT and then get it to generate a quiz that you can put straight into Quiz Maker and use for revision. The possiblities are endless!
+
+Another feature that I was able to implement with React was the "Q-Cards Display Mode". I have to clarify however that this was heavily inspired by my friend Dylan Roche's Q-Card website project. However, I made sure that mine was very basic and it is missing a lot of the features that Dylan's has. I will make sure to add a link to his site once it's been published.
+
+So that is Quiz Maker. Thanks for being interested in the site and I hope you found it useful. If you have any suggestions or other queries, feel free to contact me at seanthedev123@gmail.com. Happy Quiz Making! Bye!
+
+This Article was Last Edited: Tue 12 Dec 2023
--- src/pages/Logs.jsx
+++ src/pages/Logs.jsx
@@ -11,10 +11,71 @@ const Logs = () => {
       <p>Finished the Share Quiz Feature: Sun 3 Dec 2023</p>
       <p>Finished the Media Queries: Wed 6 Dec 2023</p>
       <p>Finished the Logs Page: Sun 10 Dec 2023</p>
+      <p>Added the About Section: Tue 12 Dec 2023</p>
+      <p>Finished the README file: Tue 12 Dec 2023</p>
       <h1>Credits</h1>
       <p>Created and Developed by Seán Osborne</p>
+      <p>Inpsiration for Q-Cards Display Mode from Dylan Roche</p>
+      <h1>Links</h1>
+      <a href="https://github.com/BitBoyTD/ReactQuizMaker">GitHub Repository</a>
+      <a href="https://bitboytd.github.io/quiz_maker/index.html">
+        Original Quiz Maker
+      </a>
+      <h1>About</h1>
+      <p>
+        Welcome to react Quiz Maker, your all in one tool for creating, sharing
+        and displaying quizzes. I am Seán Osborne and I started coding this
+        react Quiz Maker site on the 7th of October 2023, but Quiz Maker started
+        a while before that. I started working on the original Quiz Maker on the
+        24th of June 2023 using vanilla HTML, CSS and JS, mostly for the purpose
+        of improving my knowlege of JS. In a similiar way, my reason for
+        creating this one using React was to better my understanding of React.
+      </p>
+      <p>
+        Aswell as this I wanted to create a tool to help me study. When revising
+        I like to make quizzes to help me memorise information, but I would use
+        tools like google docs or sheets to create them which wasn't great, so I
+        decided to create a better way to do that. This idea turned into the
+        Quiz Maker site that you see today.
+      </p>
+      <p>
+        Using React for this project instead of vanilla JS has been a game
+        changer. I have been able to get stuff done faster and to implement more
+        advanced features than with the original Quiz Maker, such as the share
+        feature and the sidemenu to name a few.
+      </p>
+      <p>
+        The share feature I have realised can be used in a few interesting ways,
+        thanks to the quizzes being shared in a purely text format. This allows
+        users to put their quizzes in any file or form that contains text. So,
+        in theory one could back up all their quizzes in a text file on their
+        computer or in their google drive to be accessed from anywhere.
+      </p>
+      <p>
+        Another cool implication of it being all text is that it means ChatGPT
+        or other A.I. chatbots are able to create quizzes in the text format.
+        For example you can ask ChatGPT to generate a quiz about capital cities,
+        but to put it in the format of a Quiz Maker quiz{" "}
+        {"(Make sure to paste in an example of the Quiz Maker format)"}. I have
+        tried it myself and it works fantastically. It means that you could
+        paste your study material or an already made quiz into ChatGPT and then
+        get it to generate a quiz that you can put straight into Quiz Maker and
+        use for revision. The possiblities are endless!
+      </p>
+      <p>
+        Another feature that I was able to implement with React was the "Q-Cards
+        Display Mode". I have to clarify however that this was heavily inspired
+        by my friend Dylan Roche's Q-Card website project. However, I made sure
+        that mine was very basic and it is missing a lot of the features that
+        Dylan's has. I will make sure to add a link to his site once it's been
+        published.
+      </p>
       <p className="bottomBtn">
-        Inpsiration for Q-Cards Display Mode from Dylan Roche
+        So that is Quiz Maker. If you managed to find this page of the site then
+        you probably know me personally, but if you don't then good on you.
+        Thanks for visiting this site and I hope you found it useful. If you
+        have any suggestions or other queries, feel free to contact me at the
+        email address listed above this article. Happy Quiz Making! Bye!
       </p>
     </main>
   );
@@ -30,3 +91,5 @@ export default Logs;
 // Finished the Share Quiz Feature: Sun 3 Dec 2023
 // Finished the Media Queries: Wed 6 Dec 2023
 // Finished the Logs Page: Sun 10 Dec 2023
+// Added the About Section: Tue 12 Dec 2023
+// Finished the README file: Tue 12 Dec 2023
